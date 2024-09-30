4b5393769065a42022471300c26e5223c31193a8
L1kkkk <kn23-m.rudenko@nubip.edu.ua>
2024-10-01 01:05:12 +0800

    task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,7 +2,20 @@
 // на певний символ, наприклад *.
 
 function replaceVowels(str) {
-  return str.replace(/[aeiou]/gi, '*');
+  const vowels = 'aeiouAEIOU'; 
+  let result = ''; 
+
+  
+  for (let i = 0; i < str.length; i++) {
+    
+    if (vowels.includes(str[i])) {
+      result += '*';
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result; 
 }
 
 console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
