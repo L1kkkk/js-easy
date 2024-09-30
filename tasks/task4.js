d65a11afc9ae5756ba09e7a7aca83897fcd73b75
L1kkkk <kn23-m.rudenko@nubip.edu.ua>
2024-10-01 01:11:08 +0800

    task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -1,10 +1,22 @@
 // Задача: Напишіть функцію findMax, яка приймає масив чисел і повертає найбільше число в масиві.
 
 function findMax(numbers) {
-  return Math.max(...numbers);
+  if (numbers.length === 0) return undefined; 
+  
+  let max = numbers[0]; 
+
+  
+  for (let i = 1; i < numbers.length; i++) {
+
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  
+  return max; 
 }
 // Приклади використання:
 console.log(findMax([3, 5, 7, 2, 8])); // Виведе: 8
 console.log(findMax([10, 20, 5, 30])); // Виведе: 30
 
-module.exports = findMax;
+module.exports = findMax; 
